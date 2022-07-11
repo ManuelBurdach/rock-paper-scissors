3e82ae69c48dedbe66746449664c6b6bbb862b70
Manuel Burdach <manuelburdach@outlook.de>
2022-07-11 23:33:23 +0200

    Update Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
-let bla = 0;
+document
+  .querySelector(".user button:nth-of-type(1)")
+  .addEventListener("click", () => {
+    playGame(1);
+  });
+
+document
+  .querySelector(".user button:nth-of-type(2)")
+  .addEventListener("click", () => {
+    playGame(2);
+  });
+
+document
+  .querySelector(".user button:nth-of-type(3)")
+  .addEventListener("click", () => {
+    playGame(3);
+  });
+
+const result = {
+  11: "Draw",
+  12: "Lose",
+  13: "Win",
+  21: "Win",
+  22: "Draw",
+  23: "Lose",
+  31: "Lose",
+  32: "Win",
+  33: "Draw",
+};
+
+let playGame = (userSelection) => {
+  let pcSelection = Math.floor(Math.random() * 3 + 1);
+  document.querySelector("main div:nth-of-type(2)").innerHTML =
+    "<h2>" + result[`${String(userSelection)}${String(pcSelection)}`] + "</h2>";
+};
