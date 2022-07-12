20748c470d4c3bc6723c097439f7cf41f0f60db0
Manuel Burdach <manuelburdach@outlook.de>
2022-07-12 19:51:08 +0200

    Update HTML JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,20 @@ let computer = document.querySelector(".computer");
 let userBtn = document.querySelectorAll(`.user button`);
 let gamePlay = true;
 let output = document.querySelector(".result");
+let scoreOutput = document.querySelector(".score");
+let score = [0, 0, 0];
 
 const result = {
   // 1=Rock / 2=Paper / 3=Scissor / (11=Rock/Rock (Draw))
-  11: "Draw",
-  12: "Lose",
-  13: "Win",
-  21: "Win",
-  22: "Draw",
-  23: "Lose",
-  31: "Lose",
-  32: "Win",
-  33: "Draw",
+  11: ["Draw", 2],
+  12: ["Lose", 3],
+  13: ["Win", 1],
+  21: ["Win", 1],
+  22: ["Draw", 2],
+  23: ["Lose", 3],
+  31: ["Lose", 3],
+  32: ["Win", 1],
+  33: ["Draw", 2],
 };
 
 let img = [
@@ -32,58 +34,14 @@ let playGame = (userSelection) => {
   if (gamePlay) {
     let pcSelection = Math.floor(Math.random() * 3 + 1);
     gamePlay = false;
-    // userBtn[userSelection - 1].style.backgroundColor = "red";
+    score[result[String(userSelection) + String(pcSelection)][1] - 1]++;
     computer.setAttribute("src", img[pcSelection - 1]);
-    output.innerText = result[String(userSelection) + String(pcSelection)];
+    output.innerText = result[String(userSelection) + String(pcSelection)][0];
     setTimeout(() => {
       gamePlay = true;
       computer.setAttribute("src", "");
       output.innerText = "Ergebnis";
+      scoreOutput.innerText = `(Win: ${score[0]}) (Draw: ${score[1]}) (Lose: ${score[2]})`;
     }, 800);
   }
 };
-
-//                ALT !!!!!!!!!!!!!!!!!!
-// let computerBtn = document.querySelectorAll(`.computer button`);
-// let userBtn = document.querySelectorAll(`.user button`);
-// let gamePlay = true;
-// let output = document.querySelector(".result");
-
-// const result = {
-//   11: "Draw",
-//   12: "Lose",
-//   13: "Win",
-//   21: "Win",
-//   22: "Draw",
-//   23: "Lose",
-//   31: "Lose",
-//   32: "Win",
-//   33: "Draw",
-// };
-
-// userBtn.forEach((userBtn, index) => {
-//   userBtn.addEventListener("click", () => {
-//     playGame(index + 1);
-//   });
-// });
-
-// let clearBoth = () => {
-//   computerBtn.forEach((btn, index) => {
-//     btn.style.backgroundColor = "transparent";
-//     userBtn[index].style.backgroundColor = "transparent";
-//   });
-//   gamePlay = true;
-// };
-
-// let playGame = (userSelection) => {
-//   if (gamePlay) {
-//     let pcSelection = Math.floor(Math.random() * 3 + 1);
-//     gamePlay = false;
-//     computerBtn[pcSelection - 1].style.backgroundColor = "red";
-//     userBtn[userSelection - 1].style.backgroundColor = "red";
-//     output.innerHTML = result[String(userSelection) + String(pcSelection)];
-//     setTimeout(() => {
-//       clearBoth();
-//     }, 250);
-//   }
-// };
