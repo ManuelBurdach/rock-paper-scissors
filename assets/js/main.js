272971a8d13c091f6f49f8ba91d39416d1561a5d
Manuel Burdach <manuelburdach@outlook.de>
2022-07-12 17:31:15 +0200

    Update Css Html Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,48 @@
-let computerBtn = document.querySelectorAll(`.computer button`);
+// let computerBtn = document.querySelectorAll(`.computer button`);
+// let userBtn = document.querySelectorAll(`.user button`);
+// let gamePlay = true;
+// let output = document.querySelector(".result");
+
+// const result = {
+//   11: "Draw",
+//   12: "Lose",
+//   13: "Win",
+//   21: "Win",
+//   22: "Draw",
+//   23: "Lose",
+//   31: "Lose",
+//   32: "Win",
+//   33: "Draw",
+// };
+
+// userBtn.forEach((userBtn, index) => {
+//   userBtn.addEventListener("click", () => {
+//     playGame(index + 1);
+//   });
+// });
+
+// let clearBoth = () => {
+//   computerBtn.forEach((btn, index) => {
+//     btn.style.backgroundColor = "transparent";
+//     userBtn[index].style.backgroundColor = "transparent";
+//   });
+//   gamePlay = true;
+// };
+
+// let playGame = (userSelection) => {
+//   if (gamePlay) {
+//     let pcSelection = Math.floor(Math.random() * 3 + 1);
+//     gamePlay = false;
+//     computerBtn[pcSelection - 1].style.backgroundColor = "red";
+//     userBtn[userSelection - 1].style.backgroundColor = "red";
+//     output.innerHTML = result[String(userSelection) + String(pcSelection)];
+//     setTimeout(() => {
+//       clearBoth();
+//     }, 250);
+//   }
+// };
+
+let computer = document.querySelector(".computer");
 let userBtn = document.querySelectorAll(`.user button`);
 let gamePlay = true;
 let output = document.querySelector(".result");
@@ -15,29 +59,29 @@ const result = {
   33: "Draw",
 };
 
+let img = [
+  "./assets/img/rock96.png",
+  "./assets/img/papier64.png",
+  "./assets/img/scissor64.png",
+];
+
 userBtn.forEach((userBtn, index) => {
   userBtn.addEventListener("click", () => {
     playGame(index + 1);
   });
 });
 
-let clearBoth = () => {
-  computerBtn.forEach((btn, index) => {
-    btn.style.backgroundColor = "transparent";
-    userBtn[index].style.backgroundColor = "transparent";
-  });
-  gamePlay = true;
-};
-
 let playGame = (userSelection) => {
   if (gamePlay) {
     let pcSelection = Math.floor(Math.random() * 3 + 1);
     gamePlay = false;
-    computerBtn[pcSelection - 1].style.backgroundColor = "red";
-    userBtn[userSelection - 1].style.backgroundColor = "red";
-    output.innerHTML = result[String(userSelection) + String(pcSelection)];
+    // userBtn[userSelection - 1].style.backgroundColor = "red";
+    computer.setAttribute("src", img[pcSelection - 1]);
+    output.innerText = result[String(userSelection) + String(pcSelection)];
     setTimeout(() => {
-      clearBoth();
-    }, 250);
+      gamePlay = true;
+      computer.setAttribute("src", "");
+      output.innerText = "Ergebnis";
+    }, 800);
   }
 };
